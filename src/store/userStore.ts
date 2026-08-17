ac9d1c1443cbbd722992585c557cf93a4d952067
sisonjcs <jcssison1@gmail.com>
2026-08-17 22:35:21 +0800

    chore: moved prisma client declaration to a separate file (prisma.ts)

--- src/store/userStore.ts
+++ src/store/userStore.ts
@@ -1,11 +1,5 @@
 import { User } from "../generated/prisma/client";
-import { PrismaClient } from "../generated/prisma/client";
-import { PrismaPg } from "@prisma/adapter-pg";
-
-const adapter = new PrismaPg({ connectionString: process.env.DATABASE_URL });
-export const prisma = new PrismaClient({
-  adapter: adapter,
-});
+import { prisma } from "../prisma";
 
 /**
  * Creates a new user and adds it to the in-memory user list.
