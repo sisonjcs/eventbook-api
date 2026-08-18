baf948b8b60e1e602ef4acf7d7851398502c58fa
sisonjcs <jcssison1@gmail.com>
2026-08-18 09:48:48 +0800

    feat(middleware): added require auth check middleware

---
+++ src/middleware/requireAuth.ts
@@ -0,0 +1,8 @@
+import { NextFunction, Request, Response } from "express";
+
+export function requireAuth(req: Request, res: Response, next: NextFunction) {
+  if (!req.session.userId) {
+    return res.status(401).send({ error: "Not authenticated" });
+  }
+  next();
+}
