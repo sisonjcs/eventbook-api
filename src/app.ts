77f043498fa838c8961772e58d3a14775b5eee90
sisonjcs <jcssison1@gmail.com>
2026-08-30 13:28:31 +0800

    chore: split index.ts to app.ts and index.ts

---
+++ src/app.ts
@@ -0,0 +1,29 @@
+import express from "express";
+import session from "express-session";
+import { router as authRouter } from "./routes/auth";
+import { router as eventsRouter } from "./routes/events";
+import { router as bookingsRouter } from "./routes/bookings";
+
+export function createApp() {
+  const app = express();
+  app.use(express.json());
+  app.use(
+    session({
+      secret: process.env.SESSION_SECRET!,
+      resave: false,
+      saveUninitialized: false,
+      cookie: {
+        httpOnly: true,
+        /**
+         * Will revisit for deployment, false for now since this is for local dev only
+         */
+        secure: false,
+      },
+    }),
+  );
+  app.use(authRouter);
+  app.use(eventsRouter);
+  app.use(bookingsRouter);
+
+  return app;
+}
--- src/index.ts
+++ src/index.ts
@@ -1,35 +1,12 @@
-import express, { Application } from "express";
-import session from "express-session";
 import "dotenv/config";
 import { connectRedis } from "./redis";
-import { router as authRouter } from "./routes/auth";
-import { router as eventsRouter } from "./routes/events";
-import { router as bookingsRouter } from "./routes/bookings";
-
-const app: Application = express();
-
-app.use(express.json());
-app.use(
-  session({
-    secret: process.env.SESSION_SECRET!,
-    resave: false,
-    saveUninitialized: false,
-    cookie: {
-      httpOnly: true,
-      /**
-       * Will revisit for deployment, false for now since this is for local dev only
-       */
-      secure: false,
-    },
-  }),
-);
-
-app.use(authRouter);
-app.use(eventsRouter);
-app.use(bookingsRouter);
 
 async function main() {
   await connectRedis();
+
+  const { createApp } = await import("./app");
+  const app = createApp();
+
   app.listen(3000, () => {
     console.log("Listening on port 3000");
   });
